f3b3b2b021617bb0054b3092227a849e79ea9864
Giannis Fardelas <farg245@gmail.com>
2023-06-30 11:19:57 +0300

    fixed some comments

--- routes/merchantRoute.js
+++ routes/merchantRoute.js
@@ -6,7 +6,7 @@ const Admin = require("../models/admin");
 const adminAuth = require("../middlewares/adminAuth");
 
 
-// Route for viewing all orders (no admin authorization required yet )
+
 router.get("/viewall",adminAuth, merchantController.viewAllOrders);
 router.get("/completedorder/:id",adminAuth, merchantController.completeOrder);
 router.get("/pendingorders/",adminAuth, merchantController.getWaitingOrders);
